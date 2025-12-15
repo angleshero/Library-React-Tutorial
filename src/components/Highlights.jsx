5dd5c8f22419e7242c411e1babd87067dd418eac
tommy.cowart <tommylcowart@gmail.com>
2025-12-15 09:02:41 -0600

    highlight update

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,41 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+
+
+
+const Highlights = () => {
+    return (
+       <section id="highlights">
+       <div className="container">
+       <div className="row">
+        <h2 className="section__title">
+            Why choose <span className="purple">Library</span>?
+        </h2>
+        <div className="hightlight__wrapper">
+
+            <Highlight icon={<FontAwesomeIcon icon="bolt" />} 
+            title="Easy and Quick"
+            para="Get access to the book you purchased online instantly."
+            />
+
+            <Highlight icon={<FontAwesomeIcon icon="open-book" />} 
+            title="10,000+ Books"
+            para="Library has books in all your favorite categories."
+            />
+
+            <Highlight icon={<FontAwesomeIcon icon="tags" />} 
+            title="Affordable"
+            para="Get your hands on popular books for as laow as $10"
+            />
+
+                 
+
+          
+        </div>
+       </div>
+       </div>
+       </section>
+    );
+}
+
+export default Highlights;
