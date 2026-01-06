11cbdb05745c7fe41d47f3f173ff059fa23567a8
tommy.cowart <tommylcowart@gmail.com>
2026-01-06 10:31:15 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Nav from './components/nav';
 import Landing from './components/Landing';
 import Highlights from './components/Highlights';
 import Featured from './components/Featured';
+import Discounted from './components/Discounted';
+import Explore from './components/Explore';
 
 
 
@@ -13,6 +15,8 @@ function App() {
       <Landing />
       <Highlights />
       <Featured />
+      <Discounted />
+      <Explore />
 
     </div>
   );
