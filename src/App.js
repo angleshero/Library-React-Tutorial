90f544a288dfa513604707e3240094f2e3e18d26
tommy.cowart <tommylcowart@gmail.com>
2026-01-08 11:32:00 -0600

    updated app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import Nav from "./components/nav";
 import Footer from "./components/Footer";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
+
+import Home from "./pages/Home";
 import Books from "./pages/Books";
-
-
-
-
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Nav />
-      <Routes>
-        <Route path="/" exact element={Home} />
-        <Route path="/books" element={Books} />
-      </Routes>
-      
-      <Footer />
+      <div className="App">
+        <Nav />
 
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books />} />
+        </Routes>
+
+        <Footer />
+      </div>
     </Router>
   );
 }
 
-export default App; 
+export default App;
