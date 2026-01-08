3e33c0bebc11e195722eb37f87a1d5e17c6f5def
tommy.cowart <tommylcowart@gmail.com>
2026-01-08 10:50:38 -0600

    routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Books from './pages/Books';
+import { Routes } from 'react-router-dom';
 
 
 
@@ -13,8 +14,10 @@ function App() {
   return (
     <Router>
     <div className="App">
-      <Route path="/" exact component={Home} />
-      <Route path="/books" component={Books} />
+      <Routes>
+        <Route path="/" exact component={Home} />
+        <Route path="/books" component={Books} />
+      </Routes>
       <Nav />
       <Footer />
 
