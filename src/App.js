c81dc7985aa99598c91662b4aa3895a0b0f0a7b0
tommy.cowart <tommylcowart@gmail.com>
2026-01-08 10:12:32 -0600

    CAPBOOK

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import Nav from './components/nav';
-import Landing from './components/Landing';
-import Highlights from './components/Highlights';
-import Featured from './components/Featured';
-import Discounted from './components/Discounted';
-import Explore from './components/Explore';
+
 import Footer from './components/Footer';
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Books from './pages/Books';
 
 
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
+      <Route path="/" exact component={Home} />
+      <Route path="/books" component={Books} />
       <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
       <Footer />
 
     </div>
+    </Router>
   );
 }
 
