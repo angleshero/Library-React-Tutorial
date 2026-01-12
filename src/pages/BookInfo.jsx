9444d72d7073f65bc152e2e05cd2abe9ed5a7b9c
tommy.cowart <tommylcowart@gmail.com>
2026-01-12 11:10:47 -0600

    priceupdate

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Rating from '../components/ui/Rating';
+import Price from '../components/ui/Price';
 
 const BookInfo = ({ books}) => {
     return (
