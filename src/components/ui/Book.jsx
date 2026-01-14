cb7f524ad1cfab0d26507699c3416ba4b550f9d5
tommy.cowart <tommylcowart@gmail.com>
2026-01-14 10:09:40 -0600

    updates

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -7,14 +7,14 @@ import Rating from './Rating';
 const Book = ({ book }) => {
     return (
         <div className="className book">
-                            <Link to="/books/1">
+                            <Link to={`/books/${book.id}`}>
                                 <figure className="book__img--wrapper">
                                     <img src={book.url} alt="" className ="book__img"/>
 
                                     </figure>
                             </Link>
                             <div className="book__title">
-                                <Link to="/books/1" className="book__title--link">
+                                <Link to={`/books/${book.id}`} className="book__title--link">
                                 {book.title}
                                 </Link>
 
--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,10 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Rating from '../components/ui/Rating';
 import Price from '../components/ui/Price';
+import Book from '../components/ui/Book';
 
 const BookInfo = ({ books}) => {
+    const { id } = useParams();
+    const book = books.find((book) => +book.id === +id);
     return (
         <div id="books__body">
             <main id="books__main">
@@ -21,16 +24,48 @@ const BookInfo = ({ books}) => {
                         </Link>
                       </div>
                       <figure className="book__selected">
-                        <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" className="book__selected--image" />
+                        <img src={book.url} alt="" className="book__selected--image" />
                       </figure>
                       <div className="book__selected--description">
-                        <h2 className="book__selected--title">Crack the Code</h2>
-                        <Rating rating="4.5" />
+                        <h2 className="book__selected--title">{book.title}</h2>
+                        <Rating rating={book.rating} />
                         <div className="book__selected--price">
-                            <Price orginalPrice={50} salePrice={20} />
+                            <Price orginalPrice={book.originalPrice} salePrice={book.salePrice} />
+                        </div>
+                        <div className="book__summary">
+                            <h3 className="book__summary--title">
+                                Summary
+                            </h3>
+                            <p className="book__summary--para">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas iure laborum est? Amet dolorum reiciendis ad ipsum voluptates laudantium saepe cumque, molestias esse. Cum, ea doloremque perferendis optio quia quae.
+                            </p>
+                            <p className="book__summary--para">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas iure laborum est? Amet dolorum reiciendis ad ipsum voluptates laudantium saepe cumque, molestias esse. Cum, ea doloremque perferendis optio quia quae.
+                            </p>
+                            <button className="btn">
+                                Add to cart
+                            </button>
                         </div>
 
                       </div> 
+                    </div>
+                </div>
+
+                <div className="books__container">
+                    <div className="row">
+                        <div className="book__selected--top">
+                            <h2 className="book__selected--title--top">
+                                Recommended Books
+                            </h2>
+                        </div>
+                        <div className="books">
+                        {
+                            books
+                            .filter(book => book.rating === 5 && +book.id !== +id)
+                            .slice(0, 4)
+                            .map((book => <Book book={book} key={book.id} />))
+                        }
+                        </div>
                     </div>
                 </div>
             </main>
