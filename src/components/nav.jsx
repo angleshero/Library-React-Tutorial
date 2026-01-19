48774bca81e35c6c1f5c5317064f75e3e8db1165
tommy.cowart <tommylcowart@gmail.com>
2026-01-19 15:32:40 -0600

    render

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import LibraryLogo from '../assets/Library.svg';
 import { Link } from 'react-router-dom';
 
-    const Nav = ( numberOfItems) => {
+    const Nav = ( {numberOfItems}) => {
         function openMenu() {
             document.body.classList =+ "menu--open";
 
