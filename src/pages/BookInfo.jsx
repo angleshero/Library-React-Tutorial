7f1d43458645d68b694bd1f7ac2715228233972b
tommy.cowart <tommylcowart@gmail.com>
2026-01-19 14:58:31 -0600

    update

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -4,7 +4,7 @@ import { Link, useParams } from 'react-router-dom';
 import Rating from '../components/ui/Rating';
 import Price from '../components/ui/Price';
 import Book from '../components/ui/Book';
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const BookInfo = ({ books, addToCart}) => {
     const { id } = useParams();
