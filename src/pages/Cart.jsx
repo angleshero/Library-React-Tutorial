a7fa7ee6085766ddc92dc4b5743d389d6eb9bf57
tommy.cowart <tommylcowart@gmail.com>
2026-01-20 11:36:21 -0600

    update

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -18,8 +18,8 @@ const Cart = ({ cart, changeQuantity, removeItem }) => {
             <main id="books__main">
                 <div className="books__container">
                     <div className="row">
-                        <div className="book__selected--top" >
-                            <h2 className="cart__title" flex-direction="row">
+                        <div className="book__selected--top">
+                            <h2 className="cart__title">
                                 Cart
                             </h2> 
                             <div className="cart">
